9d62c331a545c2fe03a0c1fc2e09be2e88626e94
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-23 20:48:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,5 +7,5 @@ doc = Nokogiri::HTML(html)
 
 # doc.css(".headline-260IBN") 
 
-doc.css(."inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
